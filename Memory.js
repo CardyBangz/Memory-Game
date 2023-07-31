ed7d4f35f187159969bc63c96e15a3f395cb63a4
CardyBangz <133674601+CardyBangz@users.noreply.github.com>
2023-07-31 07:37:50 -0400

    Add files via upload

---
+++ Memory.js
@@ -0,0 +1,35 @@
+const CardContent= document.getElementById("colorNames")
+
+const Colors=[
+    "red",
+    "blue",
+    "green",
+    "orange",
+    "yellow",
+    "red",
+    "blue",
+    "green",
+    "orange",
+    "yellow"
+];
+
+function shuffle(array){
+    let counter=array.length;
+
+    while(counter>0){
+        let index=Math.floor(Math.random)* counter; 
+    
+
+    counter--;
+}
+return array;
+
+};
+
+
+let ColorChange= shuffle.Colors
+
+if(e.target.add.EventListener("click", function));
+const MakeNewDiv= document.createNewElement("div")
+MakeNewDiv.add.color("")
+
